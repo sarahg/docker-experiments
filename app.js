ba09dd699c838a170ab0aeca09d1645fe8a5a1d2
Sarah German <sgerman@pantheon.io>
2019-03-15 12:18:40 -0500

    Add nodemon for picking up nodejs changes from mount

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express()
 
 const port = process.env.PORT
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Sarah!'))
+
+app.get('/docker', (req, res) => {
+
+  res.send('hello from docker');
+
+});
+
+app.get('/nodemon', (req, res) => res.send('hello from nodemon'))
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
